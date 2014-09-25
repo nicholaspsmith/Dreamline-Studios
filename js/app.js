9236638830b896e6731fa77134d1cdb2a6c144c9
Nick P Smith <npsmith1990@gmail.com>
2014-09-25 07:35:53 -0500

    Get vertical scroll working

--- js/app.js
+++ js/app.js
@@ -3,5 +3,19 @@
 $(document).foundation();
 
 $(document).ready(function(){
-  $('.space').height(window.innerHeight - 100);
+  var spaces = $('.space');
+  spaces.height(window.innerHeight);
+
+  verticallyCenter($('.dreamlogo').parent());
+  verticallyCenter($('.caption').parent());
+
+  $('#fullpage').fullpage({
+    anchors: ['space-1', 'space-2', 'space-3', 'space-4', 'space-5']
+  });
+
 });
+
+function verticallyCenter($el) {
+  var top = ($el.parent().height() - $el.height()) / 2;
+  $el.css('margin-top', top);
+}
